bbbc0cefe62d73ed5d4ae4f36ec551a9132288c6
L3p1989 <phogan110@live.com>
2021-04-15 23:33:06 -0700

    removed form and button inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,13 @@ const App = () => {
 
     const [email, setEmail] = useState('');
 
-    const postEmail = e => {
-        e.preventDefault();
-        alert(`${email} submitted, thank you!`);
-    }
-
     return (
         <>
             <Greeter text="Hello there" name="L3p" />
             <Greeter text="Idk what to write here" name="oh well" />
             <Greeter text="Sorry I don't have something better to say" name="person reading this" />
-            <form> 
-                <input placeholder="Email" type="email" onChange={e => {setEmail(e.target.value)}} />
-                <button type="submit" onClick={postEmail}>Submit</button>
-                <input type="reset" />
-            </form>
+            <input placeholder="Email" type="email" onChange={e => {setEmail(e.target.value)}} />  
+            <p>You're logging in as: {email}</p>
         </>
     );
 }
