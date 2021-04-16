73e45a87706299ddc2b80fc89589b283e15de95c
L3p1989 <phogan110@live.com>
2021-04-15 23:13:28 -0700

    rendered Greeter two more times

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     return (
         <>
             <Greeter text="Hello there" name="L3p" />
+            <Greeter text="Idk what to write here" name="oh well" />
+            <Greeter text="Sorry I don't have something better to say" name="person reading this" />
         </>
     );
 }
