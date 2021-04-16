2b9b25d02561b1d82c62de208ec20ac08add64cd
L3p1989 <phogan110@live.com>
2021-04-15 23:37:37 -0700

    added loaded state with conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Greeter from './components/Greeter';
 const App = () => {
 
     const [email, setEmail] = useState('');
+    const [loaded, setLoaded] = useState(false);
 
-    return (
+    if (loaded) return (
         <>
             <Greeter text="Hello there" name="L3p" />
             <Greeter text="Idk what to write here" name="oh well" />
             <Greeter text="Sorry I don't have something better to say" name="person reading this" />
             <input placeholder="Email" type="email" onChange={e => {setEmail(e.target.value)}} />  
             <p>You're logging in as: {email}</p>
+        </>
+    );
+
+    else return (
+        <>
+            <h1>Website loading...</h1>
         </>
     );
 }
