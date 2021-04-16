f8fa50c9fb357c17bae03fd8c64aafa06d44328d
L3p1989 <phogan110@live.com>
2021-04-15 23:05:09 -0700

    corrected h2 text

--- src/components/Greeter.jsx
+++ src/components/Greeter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Greeter = props => {
     return (
         <div>
-            <h2>{props.text}, {props.name}</h2>
+            <h2>{props.text}, {props.name}!</h2>
         </div>
     );
 };
